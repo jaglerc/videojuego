7be580e30aef71914cb632900782dc2d076562d5
jaglerc <jaglerdavidcaviedesciro@gmail.com>
2026-03-17 18:56:25 -0500

    solución de problema de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,POST,PUT,DELETE',
-  });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
-}
+    const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+
+    await app.listen(3000);
+}
 bootstrap();
