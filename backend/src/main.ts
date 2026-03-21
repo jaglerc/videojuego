6bf9414c628305f7b7ca3110c4c414ca6e0c9c16
Deiiivy <deivypr28@gmail.com>
2026-03-21 12:52:12 -0500

    updated url frontend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: ['http://localhost:5173', 'https://elaborate-cajeta-84beba.netlify.app'],
+  origin: ['http://localhost:5173', 'https://game-forestpb.netlify.app'],
   methods: 'GET,POST,PUT,DELETE',
 })
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
